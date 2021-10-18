755ce5c5e20cb53c490a8bb7da08739cffccccb7
Jader Dias <52470+JaderDias@users.noreply.github.com>
2021-10-18 08:57:59 +0000

    remove unnecessary output files

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "save_chess_games_url" {
-	# Access the module output with module.<module_name>.<output_name>
-	value = module.save_chess_games.function_url
-}
-
-output "chess_to_script_url" {
-	# Access the module output with module.<module_name>.<output_name>
-	value = module.chess_to_script.function_url
-}
